cf1034e884770c0de037b946ca1ccc78230d0e1b
Mac <mac@Macs-MacBook-Pro.local>
2020-04-19 15:12:30 -0400

    more comments from pseudocode

--- leader_election.rkt
+++ leader_election.rkt
@@ -46,17 +46,28 @@ pred networkInvariant {
 -- to initialize the distances between one node to the other
 -- how do we do that ???
 pred set_distance[??]{
-    -- ?? 
+    -- ??
+    /**
+    *	for n : Nodes:
+    *        for m: Node:
+    *			n->distance->m
+    */
 }
 
 -- given a network, return all its nodes
 fun find_all_candidates[n: Network] : set Node{
     -- TODO
+    /**
+    return a list of candidates
+    */
 }
 
 -- find the closest node in n's perspective, excluding itself
 fun closest[n: Node, ns: set Node] : Node{
     -- TODO
+    /**
+    closest, not including self 
+    */
 }
 
 ----------------- Part 3. Transitions ------------
@@ -64,22 +75,52 @@ fun closest[n: Node, ns: set Node] : Node{
 -- if clock gets zero, it should timeout
 transition[State] clock_count_down {
     -- TODO:
+    /**
+    *	if clock > 0, clock --;
+    *	if clock = 0, timeout, state = candidate, clock = 100, term ++;
+    */
 }
 
 -- one Node determines if it wants to vote for a candidate
 transition[State] vote_request {
     -- TODO:
+    /**
+    *if len(candidate) > 0:
+    		one n: Nodes:
+			cands = findAllCandidates
+			m = closest[cands]
+			if (m.Term > n.Term) or (n.voteTo = null && m.Term = n.Term):
+				n.voteTo = m
+				m.voteFrom.append(n)
+				n.Term = m.Term
+				n.Clock reset
+				n.State = Follower
+    */
 }
 
 -- one candidate check to see if it can become a leader
 transition[State] become_leader {
     -- TODO:
+    /**
+     for all candidates or just selecting one candidates:
+	if voteFrom > half,
+ become leader.
+ heartbeat = 30
+ his clock never count down again, maybe he sends himself heartbeat as well
+ */
 }
 
 -- the leader's heartbeat should count down, only if
 -- there exist a leader
 transition[State] heartbeat_countdown{
     -- TODO:
+    /**
+heatbeat --
+if heartbeat == 0:
+heartbeat = 30
+for n in nodes:
+	reset clock to random between 50 - 100
+*/
 }
 
 -- we have to randomly choose one of the four transitions available
