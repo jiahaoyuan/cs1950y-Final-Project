0eac0297a3d9b709d886f5ed1ccf70e362d017fe
Mac <mac@Macs-MacBook-Pro.local>
2020-04-25 22:16:19 -0400

    another unsat bug

--- revised_simple.rkt
+++ revised_simple.rkt
@@ -47,27 +47,62 @@ state[State] initState {
 
 ------------------------------Transition--------------------------
 
- transition[State] timeout { 
-    one n : network - leaders | let next_trm = sing[add[sum[trm[n]], 1]] |{
-        -- term ++
-        trm' = trm - n->trm[n]
-        trm' = trm + n->next_trm
-        n in followers implies {
-            candidates' = candidates + n
-            followers' = followers - n
-        } else {
-            followers' = followers
-            candidates' = candidates
+-- randomly select a follower or a leader to timeout
+transition[State] timeout { 
+    one n : network - leaders | let cur_trm = trm[n] |
+        let next_trm = sing[add[sum[cur_trm], 1]] | {
+            -- term ++
+            trm' = trm - n->cur_trm + n->next_trm
+            n in followers implies {
+                candidates' = candidates + n
+                followers' = followers - n
+            } else {
+                followers' = followers
+                candidates' = candidates
+            }
         }
+        network' = network
+        step' = sing[add[sum[step], 1]]
+        leaders' = leaders
+        voteTo' = voteTo
     }
-    network' = network
-    step' = sing[add[sum[step], 1]]
-    leaders' = leaders
-    voteTo' = voteTo
+
+-- randomly select a follower and a candidate
+-- the follower should decide if it wanna to vote
+-- candidate could also fall back if its term is smaller
+transition[State] fol_comm_cand{
+    one fol: followers | one cand: candidates { 
+        sum[trm[cand]] < sum[trm[fol]]  implies {
+            -- update candidate's term to follower's term
+            trm' = trm - cand->trm[cand] + cand->trm[fol]
+            -- fallback
+            candidates' = candidates - cand
+            followers' = followers + cand
+            -- rest stay the same
+            voteTo' = voteTo
+        } else {
+            sum[trm[cand]] > sum[trm[fol]] and no voteTo[fol] implies {
+                voteTo' = voteTo + fol->cand
+                trm' = trm - fol->trm[fol] + fol->trm[cand]
+                -- rest stay the same
+                followers' = followers
+                candidates' = candidates
+            } else { -- in other situations, do nothing
+                    followers' = followers
+                    candidates' = candidates
+                    voteTo' = voteTo
+                    trm' = trm
+                }
+        }
+        network' = network
+        step' = sing[add[sum[step], 1]]
+        leaders' = leaders
+        -- if trm[cand] == trm[fol], do nothing
+    }   
 }
 
 /**
-transition[State] timeout {
+transition[State] stay_same {
     network' = network
     step' = sing[add[sum[step], 1]]
     leaders' = leaders
@@ -78,14 +113,23 @@ transition[State] timeout {
 }*/
 
 transition[State] advance {
-    timeout[this, this']
+    #candidates > 0 and #followers = 0 implies {
+        timeout[this, this']
+    }
+    #candidates = 0 and #followers > 0 implies {
+        timeout[this, this']
+    }
+    #candidates > 0 and #followers > 0 implies {
+        fol_comm_cand[this, this']
+        timeout[this, this']
+    }
 }
 ------------------------------Run----------------------
 
 trace<|State, initState, advance, _|> election {}
 
 inst bounds {
-    #Node = 3
+    #Node = 5
     #State = 3
 }
 
